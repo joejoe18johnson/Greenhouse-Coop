b290d5d71c1a75bc96739577d081be87fbcb2ea5
Johannes Johnson <69335519+joejoe18johnson@users.noreply.github.com>
2026-09-05 18:13:19 -0600

    products

--- src/lib/product-images.ts
+++ src/lib/product-images.ts
@@ -15,7 +15,7 @@ const HOVER_PRODUCT_FILES: Record<string, string> = {
   "sweetsop-red": "sweet-sop-hover-image.png",
   "sweetsop-green": "sweet-sop-hover-image.png",
   papaya: "papaya-hover-image.png",
-  "tropical-black-grape": "tropical-grape-hover-image.jpg",
+  "tropical-black-grape": "tropical-grape-hover-image.png",
 };
 
 /** Shared hover files keyed by category */
